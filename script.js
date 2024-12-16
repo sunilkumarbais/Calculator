00505402aada48fae927ebfaa6e9b2a34162e478
Sunil kumar <sunilkumarbais46@gmail.com>
2024-12-16 23:31:37 +0530

    Add project files

---
+++ script.js
@@ -0,0 +1,26 @@
+let inp = document.querySelector("input");
+
+function appendNumber(number){
+  inp.value += number;
+};
+
+function appendOperator(operator){
+  inp.value += operator;
+};
+
+function clearInp(){
+  inp.value = '';
+};
+
+function finalResult(){
+  try{
+    inp.value = eval(inp.value);
+  } catch(e){
+    inp.value = 'error';
+  }
+
+};
+
+function backSpace(){
+  inp.value = inp.value.slice( 0 , -1 );
+}
